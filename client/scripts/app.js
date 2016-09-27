a2ff0ebf5785550c574c509c00e588484f24e3a3
eseakin <eseakin@gmail.com>
2016-09-27 11:25:47 -0700

    Dragging and bringing current box to top works. Working on escaping characters.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,6 +5,7 @@ class App {
     this.index = 0;
     this.boxCount = 1;
     this.init();
+    this.zCount = 1;
     console.log('constructor');
   }
 
@@ -62,9 +63,10 @@ class App {
 
   renderMessage() {
     var i = this.index;
-    console.log(this.messages.results);
+    // console.log(this.messages.results);
     for (i; i < this.messages.results.length; i++) {
       var ele = this.messages.results[i];
+      this.index++;
 
       // for (var j = 0; j < ele.roomname.length; j++) {
       
@@ -79,10 +81,9 @@ class App {
       if (!ele.roomname || example.indexOf(ele.roomname[0].toLowerCase()) === -1) {
         // console.log(ele.roomname);
         ele.roomname = 'default';
-
       }
 
-ele.roomname = ele.roomname.replace(/\s+/g, '_');
+
 
       if (!ele.text || example.indexOf(ele.text[0].toLowerCase()) === -1) {
         // console.log(ele.text);
@@ -91,15 +92,22 @@ ele.roomname = ele.roomname.replace(/\s+/g, '_');
 
       if (!document.getElementById(ele.roomname)) {
         console.log('creating room', ele.roomname);
-        this.renderRoom(ele.roomname);
+        this.renderRoom(ele.roomname, i * 30 + 50, 200);
       }
       
-      $('#' + ele.roomname + ' .messages').append('<p>' + ele.username + ': ' + ele.text + '<p>');
+      $('#' + ele.roomname + ' .messages').append('<p></p>')
+      $('#' + ele.roomname + ' .messages p').last().text(ele.username + ': ' + ele.text);
     }
   }
 
-  renderRoom(roomname) {
+  renderRoom(roomname, x, y) {
     roomname = roomname || prompt('Name Your Room');
+
+    if (roomname === null) {
+      return;
+    }
+    //remove spaces
+    roomname = roomname.replace(/\s+/g, '_');
     
     if (document.getElementById(roomname)) {
       alert('Room already exists');
@@ -108,26 +116,35 @@ ele.roomname = ele.roomname.replace(/\s+/g, '_');
       return;
     }
 
-    if (roomname === null) {
-      return;
-    }
 
-    $('#main').append('<div class="chatbox" id='+roomname+'></div>');
+    $('#main').append('<div class="chatbox" id="'+roomname+'"></div>');
     $('#'+roomname).append('<div class="closeWindow">X</div>');
-    $('#'+roomname).append('<div class="roomname">'+roomname+'</div>');
+    $('#'+roomname).append('<div class="roomname"></div>');
+    $('#'+roomname).text(roomname);
     $('#'+roomname).append('<div class="messages"><p></p></div>');
-    $('#'+roomname).append('<div class="messageType"></div>');
+    $('#'+roomname).append('<textarea class="messageType"></textarea>');
     $('#'+roomname).append('<div class="sendButton">Send</div>');
+
+    $('#'+roomname).css('top', this.randomNum(0,window.innerHeight) + 'px');
+    $('#'+roomname).css('left', this.randomNum(0,window.innerWidth) + 'px');
+    $('#'+roomname).css('background-color', 'rgb(' + this.randomNum(0,255) + ',' + this.randomNum(0,255) + ',' + this.randomNum(0,255) + ')');
+
     
-    $(function() {
-      $('#'+roomname).draggable();
+    $(() => {
+      $('#' + roomname).draggable();
+    });
+
+    $('#' + roomname).on('click', (event) => {
+      // console.log($(event.target));
+      $('#' + roomname).css('z-index', this.zCount);
+      this.zCount++;
     });
     
     $('.sendButton').on('click', () => {
       var message = {};
-      message.username = 'eric';
-      message.roomname = 'eric';
-      message.text = 'hi this is awesome';
+      message.username = 'joker';
+      message.roomname = 'jokerface';
+      message.text = 'lasers pew pew';
       // message.text = $('.messageType').text;
       this.send(message);
       console.log('sendButton');  
@@ -150,6 +167,10 @@ ele.roomname = ele.roomname.replace(/\s+/g, '_');
   addRoom(roomName) {
 
     console.log('addRoom');
+  }
+
+  randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
 }
