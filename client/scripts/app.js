2ea739852ff2157f72a6488a91742ba7a6ae2200
eseakin <eseakin@gmail.com>
2016-09-26 21:54:08 -0700

    Got fetch working properly. New box created for every room. Text scrolls when too many lines. Need to be able to send a message!

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,18 +1,19 @@
 class App {
   constructor(server) {
     this.server = server;
+    this.messages = {};
+    this.index = 0;
+    this.boxCount = 1;
     this.init();
     console.log('constructor');
   }
 
   init() {
-    $(document).ready(function() {
-      $('.newRoomButton').on('click', function() {
-        app.renderRoom();
-      });
-
-      app.renderRoom();
+    this.fetch();
+    $('.newRoomButton').on('click', () => {
+      this.renderRoom();
     });
+    
     console.log('init');
   }
 
@@ -33,21 +34,25 @@ class App {
     });
   }
 
-  fetch(id) {
-    $.ajax({
-      // This is the url you should use to communicate with the parse API server.
-      url: 'https://api.parse.com/1/classes/messages',
-      type: 'GET',
-      data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: function (data) {
-        console.log('chatterbox: Message pulled', data);
-      },
-      error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Failed to pull message', data);
-      }
-    });
+  fetch() {
+    setInterval( ()=>{
+      $.ajax({
+        // This is the url you should use to communicate with the parse API server.
+        url: 'https://api.parse.com/1/classes/messages',
+        type: 'GET',
+        contentType: 'application/json',
+        success: (data) => {
+          this.messages = data;
+          console.log('data', this.messages);
+          this.renderMessage();
+          console.log('chatterbox: messages pulled');
+        },
+        error: (data) => {
+          // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+          console.error('chatterbox: Failed to pull message', data);
+        }
+      });
+    }, 4000);
   }
 
   clearMessages() {
@@ -55,30 +60,85 @@ class App {
     console.log('clearMessages');
   }
 
-  renderMessage(message) {
-    $('#chats').append('<div class="message">' + message.text + '</div>');
-    console.log('renderMessage');
+  renderMessage() {
+    var i = this.index;
+    console.log(this.messages.results);
+    for (i; i < this.messages.results.length; i++) {
+      var ele = this.messages.results[i];
+
+      // for (var j = 0; j < ele.roomname.length; j++) {
+      
+      // }
+
+      var example = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+      if (!ele.username || example.indexOf(ele.username[0].toLowerCase()) === -1) {
+        // console.log(ele.username);
+        ele.username = 'anonymous';
+      }
+      if (!ele.roomname || example.indexOf(ele.roomname[0].toLowerCase()) === -1) {
+        // console.log(ele.roomname);
+        ele.roomname = 'default';
+
+      }
+
+ele.roomname = ele.roomname.replace(/\s+/g, '_');
+
+      if (!ele.text || example.indexOf(ele.text[0].toLowerCase()) === -1) {
+        // console.log(ele.text);
+        ele.text = 'Nice Try';
+      }
+
+      if (!document.getElementById(ele.roomname)) {
+        console.log('creating room', ele.roomname);
+        this.renderRoom(ele.roomname);
+      }
+      
+      $('#' + ele.roomname + ' .messages').append('<p>' + ele.username + ': ' + ele.text + '<p>');
+    }
   }
 
-  renderRoom() {
-    $('#main').append('<div class="chatbox"><div class="closeWindow">X</div><div class="messages"><p></p></div><div class="messageType"></div><div class="sendButton">Send</div></div>');
+  renderRoom(roomname) {
+    roomname = roomname || prompt('Name Your Room');
+    
+    if (document.getElementById(roomname)) {
+      alert('Room already exists');
+      var roomname = prompt('Name Your Room');
+      this.renderRoom(roomname);
+      return;
+    }
+
+    if (roomname === null) {
+      return;
+    }
+
+    $('#main').append('<div class="chatbox" id='+roomname+'></div>');
+    $('#'+roomname).append('<div class="closeWindow">X</div>');
+    $('#'+roomname).append('<div class="roomname">'+roomname+'</div>');
+    $('#'+roomname).append('<div class="messages"><p></p></div>');
+    $('#'+roomname).append('<div class="messageType"></div>');
+    $('#'+roomname).append('<div class="sendButton">Send</div>');
     
     $(function() {
-      $('.chatbox').draggable();
+      $('#'+roomname).draggable();
     });
     
-    $('.sendButton').on('click', function() {
+    $('.sendButton').on('click', () => {
       var message = {};
       message.username = 'eric';
       message.roomname = 'eric';
       message.text = 'hi this is awesome';
       // message.text = $('.messageType').text;
-      app.send(message);
+      this.send(message);
       console.log('sendButton');  
     });    
 
-    $('.closeWindow').on('click', function(event) {
+    $('.closeWindow').on('click', (event) => {
       $(event.target).parent().remove();
+      //MAKE THIS WORRRRRK
+      // setTimer(function() {
+      //   $(event.target).parent().remove();
+      // }.bind(this), 1000);
     });
     console.log('renderRoom');
   }
@@ -94,15 +154,18 @@ class App {
 
 }
 
+$(document).ready( () => {
 var app = new App('https://api.parse.com/1/classes/messages');
-
 
 var m = {
   username: 'eric',
-  text: 'hi there this is a message',
-  roomname: 'Space'
+  text: 'kaboom',
+  roomname: 'wheee'
 };
 
+var mes = {results:[m,m,m,m,m,m,m]};
+
+});
 
 
 
