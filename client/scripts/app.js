dee7252f632ea66b3f16cdcfbe83e6ee32ef8510
eseakin <eseakin@gmail.com>
2016-09-27 17:34:59 -0700

    Got send working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,6 +6,7 @@ class App {
     this.boxCount = 1;
     this.init();
     this.zCount = 1;
+    this.roomname;
     console.log('constructor');
   }
 
@@ -19,6 +20,7 @@ class App {
   }
 
   send(message) {
+    console.log('sending', JSON.stringify(message));
     $.ajax({
       // This is the url you should use to communicate with the parse API server.
       url: 'https://api.parse.com/1/classes/messages',
@@ -42,6 +44,7 @@ class App {
         url: 'https://api.parse.com/1/classes/messages',
         type: 'GET',
         contentType: 'application/json',
+        data: 'order=-createdAt',
         success: (data) => {
           this.messages = data;
           console.log('data', this.messages);
@@ -53,7 +56,7 @@ class App {
           console.error('chatterbox: Failed to pull message', data);
         }
       });
-    }, 4000);
+    }, 2000);
   }
 
   clearMessages() {
@@ -68,10 +71,6 @@ class App {
       var ele = this.messages.results[i];
       this.index++;
 
-      // for (var j = 0; j < ele.roomname.length; j++) {
-      
-      // }
-
       var example = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
       if (!ele.username || example.indexOf(ele.username[0].toLowerCase()) === -1) {
@@ -83,8 +82,6 @@ class App {
         ele.roomname = 'default';
       }
 
-
-
       if (!ele.text || example.indexOf(ele.text[0].toLowerCase()) === -1) {
         // console.log(ele.text);
         ele.text = 'Nice Try';
@@ -92,11 +89,10 @@ class App {
 
       if (!document.getElementById(ele.roomname)) {
         console.log('creating room', ele.roomname);
-        this.renderRoom(ele.roomname, i * 30 + 50, 200);
+        this.renderRoom(ele.roomname);
       }
-      
-      $('#' + ele.roomname + ' .messages').append('<p></p>')
-      $('#' + ele.roomname + ' .messages p').last().text(ele.username + ': ' + ele.text);
+      $('div[id=\"'+ele.roomname+'\"]' + ' .messages').append('<p></p>')
+      $('div[id=\"'+ele.roomname+'\"]' + ' .messages p').last().text(ele.username + ': ' + ele.text);
     }
   }
 
@@ -107,7 +103,8 @@ class App {
       return;
     }
     //remove spaces
-    roomname = roomname.replace(/\s+/g, '_');
+    // roomname = roomname.replace(/\s+/g, '_');
+    // roomname = roomname.replace(/"/g, '\\\"');
     
     if (document.getElementById(roomname)) {
       alert('Room already exists');
@@ -116,38 +113,42 @@ class App {
       return;
     }
 
+    $('#main').append('<div class="chatbox" id=\"'+roomname+'\"></div>');
+    $('div[id=\"'+roomname+'\"]').append('<div class="closeWindow">X</div>');
+    $('div[id=\"'+roomname+'\"]').append('<div class="roomname"></div>');
+    $('div[id=\"'+roomname+'\"] .roomname').text(roomname);
+    $('div[id=\"'+roomname+'\"]').append('<div class="messages"></div>');
+    $('div[id=\"'+roomname+'\"]').append('<textarea class="messageType"></textarea>');
+    $('div[id=\"'+roomname+'\"]').append('<div class="sendButton">Send</div>');
 
-    $('#main').append('<div class="chatbox" id="'+roomname+'"></div>');
-    $('#'+roomname).append('<div class="closeWindow">X</div>');
-    $('#'+roomname).append('<div class="roomname"></div>');
-    $('#'+roomname).text(roomname);
-    $('#'+roomname).append('<div class="messages"><p></p></div>');
-    $('#'+roomname).append('<textarea class="messageType"></textarea>');
-    $('#'+roomname).append('<div class="sendButton">Send</div>');
-
-    $('#'+roomname).css('top', this.randomNum(0,window.innerHeight) + 'px');
-    $('#'+roomname).css('left', this.randomNum(0,window.innerWidth) + 'px');
-    $('#'+roomname).css('background-color', 'rgb(' + this.randomNum(0,255) + ',' + this.randomNum(0,255) + ',' + this.randomNum(0,255) + ')');
+    $('div[id=\"'+roomname+'\"]').css('top', this.randomNum(0,window.innerHeight - 300) + 'px');
+    $('div[id=\"'+roomname+'\"]').css('left', this.randomNum(0,window.innerWidth - 300) + 'px');
+    $('div[id=\"'+roomname+'\"]').css('background-color', 'rgb(' + this.randomNum(0,200) + ',' + this.randomNum(0,200) + ',' + this.randomNum(0,200) + ')');
 
     
     $(() => {
-      $('#' + roomname).draggable();
+      $('div[id=\"'+roomname+'\"]').draggable();
     });
 
-    $('#' + roomname).on('click', (event) => {
+    $('div[id=\"'+roomname+'\"]').on('click', (event) => {
       // console.log($(event.target));
-      $('#' + roomname).css('z-index', this.zCount);
+      $('div[id=\"'+roomname+'\"]').css('z-index', this.zCount);
       this.zCount++;
     });
     
-    $('.sendButton').on('click', () => {
+    $('.sendButton').on('click', (event) => {
       var message = {};
+      // message.username = 'joker';
+      // message.roomname = 'jokerface';
+      // message.text = 'lasers pew pew';
+      // console.log('clicked send on ',$(event.target).parent().attr('id'));
+      message.text = $(event.target).siblings('.messageType').val();
       message.username = 'joker';
-      message.roomname = 'jokerface';
-      message.text = 'lasers pew pew';
-      // message.text = $('.messageType').text;
+      message.roomname = $(event.target).parent().attr('id');
+      $(event.target).siblings('.messageType').val('');
+      // console.log('sending this ',message)
       this.send(message);
-      console.log('sendButton');  
+      console.log('sendButton');
     });    
 
     $('.closeWindow').on('click', (event) => {
@@ -177,15 +178,6 @@ class App {
 
 $(document).ready( () => {
 var app = new App('https://api.parse.com/1/classes/messages');
-
-var m = {
-  username: 'eric',
-  text: 'kaboom',
-  roomname: 'wheee'
-};
-
-var mes = {results:[m,m,m,m,m,m,m]};
-
 });
 
 
